08b6fcfd3558ecb78eae31315db0e155a03b8562
Lorenzo Di Roberto <l.diroberto@outlook.it>
2020-05-12 14:00:28 +0200

    Aggiungere i file di progetto.

---
+++ piz_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\OneDrive\Python\Codice\Python Crash Course\WebSite\Pizzeria\piz_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
